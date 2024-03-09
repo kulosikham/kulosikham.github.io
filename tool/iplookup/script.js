4a22528acec24c835fecf2d803c2c34a87b9f811
SIKHAM~ <95176597+kulosikham@users.noreply.github.com>
2024-03-09 23:28:08 +0700

    Update script.js

--- tool/iplookup/script.js
+++ tool/iplookup/script.js
@@ -7,24 +7,24 @@ document.getElementById('ipForm').addEventListener('submit', function (e) {
             const resultDiv = document.getElementById('result');
             resultDiv.innerHTML = ''; // Clear previous content
             const text = `
-                IP Address: ${data.ip}
-                Hostname: ${data.hostname || 'N/A'}
-                City: ${data.city || 'N/A'}
-                Region: ${data.region || 'N/A'}
-                Country: ${data.country_name || 'N/A'}
-                Postal Code: ${data.postal || 'N/A'}
-                Latitude: ${data.latitude || 'N/A'}
-                Longitude: ${data.longitude || 'N/A'}
-                Timezone: ${data.timezone || 'N/A'}
-                ISP: ${data.org || 'N/A'}
-                ASN: ${data.asn || 'N/A'}
-                Organization: ${data.org || 'N/A'}
-                Language: ${navigator.language || 'N/A'}
-                Browser: ${navigator.userAgent || 'N/A'}
-                Platform: ${navigator.platform || 'N/A'}
-                Screen Resolution: ${window.screen.width}x${window.screen.height} pixels
-                Color Depth: ${window.screen.colorDepth}-bit
-                Cookie Enabled: ${navigator.cookieEnabled ? 'Yes' : 'No'}
+                IP Address: ${data.ip}\n
+                Hostname: ${data.hostname || 'N/A'}\n
+                City: ${data.city || 'N/A'}\n
+                Region: ${data.region || 'N/A'}\n
+                Country: ${data.country_name || 'N/A'}\n
+                Postal Code: ${data.postal || 'N/A'}\n
+                Latitude: ${data.latitude || 'N/A'}\n
+                Longitude: ${data.longitude || 'N/A'}\n
+                Timezone: ${data.timezone || 'N/A'}\n
+                ISP: ${data.org || 'N/A'}\n
+                ASN: ${data.asn || 'N/A'}\n
+                Organization: ${data.org || 'N/A'}\n
+                Language: ${navigator.language || 'N/A'}\n
+                Browser: ${navigator.userAgent || 'N/A'}\n
+                Platform: ${navigator.platform || 'N/A'}\n
+                Screen Resolution: ${window.screen.width}x${window.screen.height} pixels\n
+                Color Depth: ${window.screen.colorDepth}-bit\n
+                Cookie Enabled: ${navigator.cookieEnabled ? 'Yes' : 'No'}\n
             `;
             typeText(resultDiv, text, 0);
         })
