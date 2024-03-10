3784f8fe19123fa6c2fa57eb12b383601303531e
SIKHAM~ <95176597+kulosikham@users.noreply.github.com>
2024-03-10 12:27:20 +0700

    Create script.js

---
+++ prd/script.js
@@ -0,0 +1,19 @@
+// JavaScript code to handle automatic product filtering
+document.addEventListener('DOMContentLoaded', function () {
+    const searchInput = document.querySelector('.form-control');
+    const products = document.querySelectorAll('.produk-box');
+
+    searchInput.addEventListener('input', function () {
+        const searchTerm = searchInput.value.toLowerCase();
+
+        products.forEach(function (product) {
+            const productName = product.querySelector('p:first-child').textContent.toLowerCase();
+
+            if (productName.includes(searchTerm)) {
+                product.style.display = 'block';
+            } else {
+                product.style.display = 'none';
+            }
+        });
+    });
+});
