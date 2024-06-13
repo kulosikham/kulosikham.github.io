77d0b23948c47e8f2c23f5cd87f698e97dc9d79d
SIKHAM~ <95176597+kulosikham@users.noreply.github.com>
2024-06-13 17:44:10 +0700

    Delete a/b/c/wind/js directory

--- a/b/c/wind/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Add your JavaScript code here
-  const searchButton = document.querySelector('button');
-  searchButton.addEventListener('click', () => {
-    const searchText = document.querySelector('input[type="text"]').value;
-    alert(`Anda mencari: ${searchText}`);
-  });
-});
