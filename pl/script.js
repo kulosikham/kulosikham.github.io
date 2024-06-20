e2a79ea329a0a5fb10d943d22c36cb5cfcfb5ac9
SIKHAM~ <95176597+kulosikham@users.noreply.github.com>
2024-06-20 13:57:32 +0700

    Update script.js

--- pl/script.js
+++ pl/script.js
@@ -1,3 +1,17 @@
+document.getElementById('lang-en').addEventListener('click', function() {
+    switchLanguage('en');
+});
+
+document.getElementById('lang-id').addEventListener('click', function() {
+    switchLanguage('id');
+});
+
+function switchLanguage(language) {
+    document.querySelectorAll('[data-en]').forEach(function(element) {
+        element.textContent = element.getAttribute(`data-${language}`);
+    });
+}
+
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
