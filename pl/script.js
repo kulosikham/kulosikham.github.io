0c7cb6dade6f010175a1b5e7de565d92ce74ad59
SIKHAM~ <95176597+kulosikham@users.noreply.github.com>
2024-06-20 14:07:29 +0700

    Update script.js

--- pl/script.js
+++ pl/script.js
@@ -1,31 +1,16 @@
-document.getElementById('lang-en').addEventListener('click', function() {
-    switchLanguage('en');
-});
-
-document.getElementById('lang-id').addEventListener('click', function() {
-    switchLanguage('id');
-});
-
-function switchLanguage(language) {
-    document.querySelectorAll('[data-en]').forEach(function(element) {
-        element.textContent = element.getAttribute(`data-${language}`);
-    });
-}
-
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
     // Validate input
-    if (!name || !email || !message) {
+    if (!name || !message) {
         alert('Please fill in all fields.');
         return;
     }
 
     // Handle form submission (e.g., send data to server)
-    console.log('Form submitted!', { name, email, message });
+    console.log('Form submitted!', { name, message });
 
     // Reset the form
     document.getElementById('contact-form').reset();
